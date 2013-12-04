71f26f4fd8d74fa7fb69c30fee4fc482648772ce
Philipe Fatio <me@phili.pe>
2013-12-04 01:03:43 +0100

    Fix spacing issues in prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,7 +1,7 @@
 # Substitute color vars in PROMPT var.
 setopt prompt_subst
 
-PROMPT='$fg[magenta]%n$reset_color@$fg[yellow]$(hostname -s)$reset_color $fg_bold[green]%~$reset_color $(git_prompt_info)❯ '
+PROMPT='%{$fg[magenta]%}%n%{$reset_color%}@%{$fg[yellow]%}$(hostname -s)%{$reset_color%} %{$fg_bold[green]%}%~%{$reset_color%} $(git_prompt_info)> '
 
 # Returns the current branch name and its status using colored dots as indicator.
 function git_prompt_info() {
@@ -16,5 +16,5 @@ function git_prompt_info() {
   local modified="$(awk '/^.[MD] / { print "•"; exit }' <<< $s)"
   local untracked="$(awk '/^?? / { print "•"; exit }' <<< $s)"
   local conflict="$(awk '/^UU / { print "•"; exit }' <<< $s)"
-  echo "$fg_bold[blue]$branch$reset_color@$fg[yellow]$sha$fg[green] $ahead$fg[red]$behind\n$fg[green]$staged$fg[red]$modified$fg[yellow]$untracked$fg[blue]$conflict$reset_color";
+  echo "%{$fg_bold[blue]%}%{$branch%}%{$reset_color%}@%{$fg[yellow]%}$sha%{$fg[green]%} $ahead%{$fg[red]%}$behind\n%{$fg[green]%}$staged%{$fg[red]%}$modified%{$fg[yellow]%}$untracked%{$fg[blue]%}$conflict%{$reset_color%}";
 }
