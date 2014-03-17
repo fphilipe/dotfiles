509449187fcb9539ab375432e4c01313b4da3522
Philipe Fatio <me@phili.pe>
2014-03-17 07:58:42 +0100

    Preserve ./bin in $PATH after chruby

--- zsh/config/tools.zsh
+++ zsh/config/tools.zsh
@@ -2,6 +2,19 @@
 # a .ruby-version file.
 source '/usr/local/share/chruby/chruby.sh'
 source '/usr/local/share/chruby/auto.sh'
+
+save_function()
+{
+  local ORIG_FUNC="$(declare -f $1)"
+  local NEWNAME_FUNC="$2${ORIG_FUNC#$1}"
+  eval "$NEWNAME_FUNC"
+}
+save_function chruby orig_chruby
+chruby() {
+  orig_chruby $*
+  PATH=./bin:$PATH
+}
+
 chruby ruby
 
 # Link homebrew cask apps in /Applications.
