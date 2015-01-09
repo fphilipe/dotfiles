e60f6b06eccff9a7419d2748235553da4da8898c
Philipe Fatio <me@phili.pe>
2015-01-09 18:19:51 +0100

    Fix wikipedia SVG image script for retina screen

--- js/wikipedia.org.js
+++ js/wikipedia.org.js
@@ -1,5 +1,6 @@
 // Replace all SVGs that are rendered as PNGs with the original SVG file.
-var imgs = Array.prototype.slice.apply(document.getElementsByTagName('img'))
+var imgs = [].slice.apply(document.getElementsByTagName('img'))
 imgs.forEach(function(img) {
-  img.src = img.src.replace(/\/thumb(\/.+)\/\d+px-.+\.svg\.png$/, '$1')
+  img.src = img.src.replace(/\/thumb(\/.+)\/\d+px-.+\.svg\.png.*$/, '$1')
+  img.removeAttribute('srcset')
 })
