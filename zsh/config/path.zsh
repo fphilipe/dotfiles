afb2cc57b63f599b7606c662bfe34343e6850c5d
Philipe Fatio <me@phili.pe>
2015-01-09 18:25:03 +0100

    Remove python path

--- zsh/config/path.zsh
+++ zsh/config/path.zsh
@@ -7,8 +7,6 @@ paths=(
   # Node Packace Manager
   '/usr/local/share/npm/bin'
   './node_modules/.bin'
-  # Python virtualenv
-  "$HOME/.virtualenv/default_env/bin"
   # Heroku toolbelt
   '/usr/local/heroku/bin'
   # Bundler binstubs
