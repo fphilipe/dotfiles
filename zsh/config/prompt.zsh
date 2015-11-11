a547873cd1beb42be12927672f652b913a2655c7
Philipe Fatio <me@phili.pe>
2015-11-11 13:12:05 +0100

    Improve branch parsing

--- zsh/config/prompt.zsh
+++ zsh/config/prompt.zsh
@@ -9,7 +9,7 @@ function git_prompt_info() {
   [ "$s" = '' ] && return
   local sha="$(git rev-parse --short HEAD 2> /dev/null)"
   local first_line="$(head -n1 <<< $s)"
-  local branch="$(awk -F'\\s+|\\.\\.\\.' '{ print $2 }' <<< "$first_line")"
+  local branch="$(<<< "$first_line" cut -d' ' -f2 | cut -d. -f1)"
   local ahead="$(sed -e '/ahead/!d' -e 's/.*ahead \([0-9]*\).*/+\1/' <<< $first_line)"
   local behind="$(sed -e '/behind/!d' -e 's/.*behind \([0-9]*\).*/-\1/' <<< $first_line)"
   local staged="$(awk '/^[MDA]. / { print "•"; exit }' <<< $s)"
