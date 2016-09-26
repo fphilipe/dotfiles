cf763a8adff7189ca2619719d57d00fc68cb8a27
Philipe Fatio <me@phili.pe>
2016-09-26 14:26:38 +0200

    Bind ^U in ZSH to behave like readline

--- zsh/config/key_bindings.zsh
+++ zsh/config/key_bindings.zsh
@@ -8,6 +8,9 @@ bindkey -e
 bindkey '^P' up-line-or-search
 bindkey '^N' down-line-or-search
 
+# Only kill from start to cursor instead of whole line.
+bindkey '^U' backward-kill-line
+
 # Make space automatically expand stuff such as "sudo !!".
 bindkey ' ' magic-space
 
