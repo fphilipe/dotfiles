5b3e07b3c7d5a20caaa75e2b60d5397f74636f96
Philipe Fatio <me@phili.pe>
2017-01-06 16:57:54 +0100

    Increase ZSH history size

--- zsh/config/history.zsh
+++ zsh/config/history.zsh
@@ -1,13 +1,14 @@
 if [ -z $HISTFILE ]; then
   HISTFILE=$HOME/.zsh_history
 fi
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=100000
+SAVEHIST=100000
 
 setopt append_history
 setopt extended_history
 setopt hist_expire_dups_first
 setopt hist_ignore_dups
+setopt hist_ignore_all_dups
 setopt hist_ignore_space
 setopt hist_verify
 setopt inc_append_history
