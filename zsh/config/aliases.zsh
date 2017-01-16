b4496b36abbca7af72d7344c72413abe6fdef388
Philipe Fatio <me@phili.pe>
2017-01-16 10:46:41 +0100

    ZSH: Remove unused aliases

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -4,7 +4,3 @@ alias ls='ls -lah'
 alias tmux='tmux -S /tmp/tmux'
 # Quicklook a file from terminal.
 alias ql='qlmanage -p "$@" &> /dev/null'
-# Reload zshrc.
-alias reload!='. ~/.zshrc'
-# Homebrew package which corrects your previous console command.
-alias fuck='eval $(thefuck $(fc -ln -1 | tail -n 1)); fc -R'
