081790bbfd8271cf5f229150d0f325d44054b862
Philipe Fatio <me@phili.pe>
2017-12-08 23:41:54 +0100

    Wrap prompt also outside git repos

--- zsh/config/prompt.zsh
+++ zsh/config/prompt.zsh
@@ -1,7 +1,7 @@
 # Substitute color vars in PROMPT var.
 setopt prompt_subst
 
-PROMPT='%D{%m/%d %H:%M} %{$fg[magenta]%}%n%{$reset_color%}@%{$fg[yellow]%}$(hostname -s) %{$fg_bold[green]%}%~%{$reset_color%} $(git_prompt_info)❯ '
+PROMPT='%D{%m/%d %H:%M} %{$fg[magenta]%}%n%{$reset_color%}@%{$fg[yellow]%}$(hostname -s) %{$fg_bold[green]%}%~%{$reset_color%} ${$(git_prompt_info)%.}❯ '
 TMOUT=60
 TRAPALRM() {
   zle reset-prompt
@@ -10,7 +10,7 @@ TRAPALRM() {
 # Returns the current branch name and its status using colored dots as indicator.
 function git_prompt_info() {
   local s="$(git status --short --branch --porcelain 2> /dev/null)"
-  [ "$s" = '' ] && return
+  [ "$s" = '' ] && echo "\n." && return
   local sha="$(git rev-parse --short HEAD 2> /dev/null)"
   local first_line="$(head -n1 <<< $s)"
   local branch="$(<<< "$first_line" cut -d' ' -f2 | cut -d. -f1)"
