c14645e4b59db273b16b854be596552ae779f24f
Philipe Fatio <me@phili.pe>
2019-10-08 18:57:14 +0200

    Add alias for git without pager

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -5,6 +5,7 @@ alias tmux='tmux -S /tmp/tmux'
 # Quicklook a file from terminal.
 alias ql='qlmanage -p "$@" &> /dev/null'
 alias g="git"
+alias gn="git --no-pager"
 # Use Android Studio's keytool instead of global keytool that would require
 # installing a Java runtime.
 alias keytool="/Applications/Android\ Studio.app/Contents/jre/jdk/Contents/Home/bin/keytool"
