58a60fadb4e6db0c7a131c1ad27031db90511f37
Philipe Fatio <me@phili.pe>
2022-06-23 22:21:25 +0200

    Tweak ZSH config

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -1,5 +1,5 @@
 # Use list style, list everything, show size with units.
-alias ls='ls -lah'
+alias ls='ls -lahp'
 # Always use the same socket.
 alias tmux='tmux -S /tmp/tmux'
 # Quicklook a file from terminal.
@@ -8,4 +8,4 @@ alias g="git"
 alias gn="git --no-pager"
 # Use Android Studio's keytool instead of global keytool that would require
 # installing a Java runtime.
-alias keytool="/Applications/Android\ Studio.app/Contents/jre/jdk/Contents/Home/bin/keytool"
+alias keytool="/Applications/Android\ Studio.app/Contents/jre/Contents/Home/bin/keytool"
