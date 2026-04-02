5d29e560912260a161b2cc2e2e27db7f0ce5816f
Philipe Fatio <me@phili.pe>
2026-04-02 21:32:10 +0200

    Fix base16 installation

--- zsh/config/colors.zsh
+++ zsh/config/colors.zsh
@@ -7,4 +7,4 @@ export LSCOLORS="Gxfxcxdxbxegedabagacad"
 
 # Base16 Shell
 BASE16_SHELL=$HOME/.config/base16-shell/
-[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
+[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && source "$BASE16_SHELL/profile_helper.sh"
