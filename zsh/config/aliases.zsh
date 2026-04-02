b93ccd3d8c7d4527b65b37c6aad88e626b37a3bb
Philipe Fatio <me@phili.pe>
2026-04-02 07:16:35 +0200

    Add alias to cd to repos root

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -9,6 +9,8 @@ alias gn="git --no-pager"
 # ripgrep with just the matches.
 alias rgm='rg --only-matching --no-filename --no-line-number --no-heading'
 alias rgf='rg --files-with-matches'
+# cd to repo root dir.
+alias cdr='cd "$(git rev-parse --show-cdup)"'
 # Use Android Studio's keytool instead of global keytool that would require
 # installing a Java runtime.
 alias keytool="/Applications/Android\ Studio.app/Contents/jbr/Contents/Home/bin/keytool"
