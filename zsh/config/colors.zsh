c4f0ca87119c729e3a6a5c7377755ae2835cb7f5
Philipe Fatio <me@phili.pe>
2026-04-20 20:41:17 +0200

    Add base16_random function

--- zsh/config/colors.zsh
+++ zsh/config/colors.zsh
@@ -8,3 +8,10 @@ export LSCOLORS="Gxfxcxdxbxegedabagacad"
 # Base16 Shell
 BASE16_SHELL=$HOME/.config/base16-shell/
 [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && source "$BASE16_SHELL/profile_helper.sh"
+
+base16_random() {
+  local scripts=($BASE16_SHELL/scripts/base16-*.sh)
+  local script=$scripts[RANDOM%${#scripts[@]}+1]
+  eval "${${script:t:r}/base16-/base16_}"
+  echo "$BASE16_THEME"
+}
